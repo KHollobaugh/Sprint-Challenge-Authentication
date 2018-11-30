b58baee9871b6409054ff51316b25e66e6ddd4fa
KHollobaugh <khollobaugh@gmail.com>
2018-11-30 13:45:12 -0500

    Added register and login components

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,88 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {Route, Switch, NavLink} from 'react-router-dom';
+import {withRouter} from 'react-router';
+import axios from 'axios';
 import './App.css';
+import Register from './components/Register';
+import Login from './components/Login';
+
+
+const url = 'http://localhost:3300';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      loggedIn: false,
+      jokes: []
+    }
+  }
+
+authenticate = () => {
+  const token = localStorage.getItem('secret_token')
+  const options = {
+    headers: {
+      Authorization: token,
+    }
+  }
+  if(token){
+    axios.get(`${url}/api/jokes`, options)
+    .then((res) => {
+      if (res.data) {
+        this.setState({ loggedIn: true, jokes: res.data})
+      } else {
+        throw new Error();
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      this.props.history.push('/login');
+    })
+  } else {
+    this.props.history.push('/login');
+    }
+}
+
+componentDidMount(){
+  this.authenticate();
+}
+
+componentDidUpdate(prevProps) {
+  const {pathname} = this.props.location;
+  if(pathname === '/' && pathname !== prevProps.location.pathname) {
+    this.authenticate();
+  }
+}
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <nav>
+          <NavLink to='/'>Home</NavLink>
+          <NavLink to='/login'>Log In</NavLink>
+          <NavLink to='/register'>Register</NavLink>
+        </nav>
+        <Switch>
+          <Route path = '/register' component={Register}/>
+          <Route path = '/login' component={Login}/>
+          <Route path = '/' render={() => {
+            return (
+              <React.Fragment>
+              <h2>Jokes!</h2>
+              <div>
+                {this.state.jokes.map(joke => 
+                <div key={joke.id}>
+                  <h4>{joke.setup}</h4>
+                  <p>{joke.punchline}</p>
+                  </div>)}
+              </div>
+              </React.Fragment>
+            )
+          }}/>
+          </Switch>
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
