aca3f66c38e5a752f06cae2c958fb280d4c6fae7
KHollobaugh <khollobaugh@gmail.com>
2018-11-30 12:09:04 -0500

    added routes

--- config/routes.js
+++ config/routes.js
@@ -1,5 +1,13 @@
+const express = require('express');
 const axios = require('axios');
+const bcrypt = require('bcryptjs');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const db = require('../database/dbConfig');
+const server = express();
 
+server.use(express.json());
+server.use(cors());
 const { authenticate } = require('./middlewares');
 
 module.exports = server => {
@@ -10,16 +18,38 @@ module.exports = server => {
 
 function register(req, res) {
   // implement user registration
+  const cred = req.body;
+  const hash = bcrypt.hashSync(creds.password, 3);
+  creds.password = hash;
+  db('users')
+  .insert(creds)
+  .then((ids) => {
+    res.status(200).json(ids);
+  })
+  .catch(err => res.status(400).json({ message: 'unable to register', ids}))
 }
 
 function login(req, res) {
   // implement user login
+  const creds = req.body;
+  db('users')
+  .where({ username: creds.username })
+  .first()
+  .then(user => {
+    if(user && bcrypt.compareSync(creds.password, user.password)){
+      const token = generateToken(user);
+      res.status(200).json({ message: 'login successful', token})
+    } else {
+      res.status(401).json({ message: 'login failed'})
+    }
+  })
+  .catch(err => res.send(err));
 }
 
 function getJokes(req, res) {
   axios
     .get(
-      'https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_ten'
+      'https://safe-falls-22549.herokuapp.com/random_ten'
     )
     .then(response => {
       res.status(200).json(response.data);
